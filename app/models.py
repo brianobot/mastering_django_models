2d6ef56b68af5b5f1bf396448ba818658f61b3dc
Brian Obot <brianobot9@gmail.com>
2024-02-04 08:47:05 +0100

    Add instruction concerning model fields naming

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Person(models.Model):
+    # fields translate to database table columns
+    # when naming fields avoid names that conflict with the model api (clean, delete, save etc)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
